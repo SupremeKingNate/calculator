e06c5904c2bc54804106ca6e9bcf11adcff78747
Nathan Saipaia <nathan.saipaia@warriorlife.net>
2017-12-07 14:55:49 -0800

    javascript
    started on the javascript

--- calculator.js
+++ calculator.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+
+//Change Display
+function d(val) {
+	document.getElementById("d") .value = val;
+}
+
+// Type numbers and operators
+function v(val) {
+	document.getElementById("d"). value+= \
+	al;
